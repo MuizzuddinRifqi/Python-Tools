124ffe7c210269ad1d0196c48c0e9138a6d40951
MuizzuddinRifqi <muizzuddinrifqi@gmail.com>
2023-11-28 13:35:29 +0700

    add termcolor for different open and close port

--- port_scanner.py
+++ port_scanner.py
@@ -1,4 +1,5 @@
 import socket
+from termcolor import colored
 
 def scan_port(ip, port):
     try:
@@ -8,13 +9,14 @@ def scan_port(ip, port):
         service_version = sock.recv(1024)
         service_version = service_version.decode('utf-8')
         service_version = service_version.strip('\n')
-        print(f'Port {str(port)}    open', end='      ')
+        portstate = f'Port {str(port)}    open'
+        print(colored(portstate, 'green'), end ='      ')
         print(f'{service_version}')
         
     except ConnectionRefusedError:
-        print(f'Port {str(port)}    closed')
+        print(colored(f'Port {str(port)}    closed', 'red'))
     except UnicodeDecodeError:
-        print(f'Port {str(port)}    open')
+        print(colored(f'Port {str(port)}    open', 'green'))
 
 target = input('[+] Target IP: ')
 ports = (input('[+] Port: '))
